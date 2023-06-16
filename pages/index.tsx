ad888f80ea7b2c19efa06ee47a6c09801782c763
jessika <1355943+jessika@users.noreply.github.com>
2023-06-16 11:08:08 -0700

    Add description and external links to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
+import styles from './index.module.css'
 
 export default function Home() {
   return (
@@ -8,11 +8,15 @@ export default function Home() {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Hi, I'm Jessika</p>
+      <section className={styles.blurb}>
+        <h2>Hi, I'm Jessika Wu</h2>
         <p>
-          The content for this website is in progress!
+          I'm a software engineer with 7 years of professional experience, with a focus on full-stack and frontend development.
         </p>
+        <div className={styles.icons}>
+          <a href="https://github.com/jessika"><img src="/images/icons/brand-github.svg" alt="Github icon" /></a>
+          <a href="https://linkedin.com/jessikawu1"><img src="/images/icons/brand-linkedin.svg" alt="LinkedIn icon" /></a>
+        </div>
       </section>
     </Layout>
   )
