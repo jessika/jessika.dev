b012116b2b75a62e1e7abbee4cd7604848fdc0e5
jessika <1355943+jessika@users.noreply.github.com>
2023-06-26 09:24:45 -0700

    Use nord color theme for code blocks

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import "../styles/global.scss";
 import { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/prism-vsc-dark-plus.css";
+import "../styles/prism-nord.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
